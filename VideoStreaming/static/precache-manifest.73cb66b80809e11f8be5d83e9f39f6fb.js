0b31525d8f19a1b8838a1293f31a227bfe499254
karanhere3962 <karanhere3962@gmail.com>
2020-05-26 02:16:48 +0530

    Docker support complete

---
+++ VideoStreaming/static/precache-manifest.73cb66b80809e11f8be5d83e9f39f6fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1061dd254f807f24766f5f53c0a8531a",
+    "url": "/static/build/index.html"
+  },
+  {
+    "revision": "3302b0383b6dab2b8940",
+    "url": "/static/build/static/css/main.30667fcf.chunk.css"
+  },
+  {
+    "revision": "562940b99001c51f665b",
+    "url": "/static/build/static/js/2.299e78e1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/build/static/js/2.299e78e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3302b0383b6dab2b8940",
+    "url": "/static/build/static/js/main.cd25862c.chunk.js"
+  },
+  {
+    "revision": "4377c86b503994d17f99",
+    "url": "/static/build/static/js/runtime-main.c6d4f993.js"
+  }
+]);
